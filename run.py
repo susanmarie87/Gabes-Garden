02b99b09b8ac13e5916c99217e2b21910cdf0315
susanmarie87 <susan.marie87@gmail.com>
2021-07-06 19:30:33 +0000

    deleted index file and static folder as they were created before I knew what to do correctly

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
-from flask import flask
+import os
+from flask import Flask
 
 
 app = Flask(__name__)
@@ -7,3 +8,10 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "Hello, World"
+
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True)
